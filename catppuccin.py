16753c2ba8d61499354ab5f08eb15390877c3f85
claymorwan <claymorwan@fembois.dev>
2025-01-28 11:49:16 +0000

    bwa

---
+++ catppuccin.py
@@ -0,0 +1,10 @@
+from pygments.style import Style
+from pygments.token import token, Comment, Keyword, Name, String, Error, Generic, Number, Operator
+
+__all__ = ['CatppuccinMochaStyle']
+
+class CatppuccinMochaStyle(Style):
+
+    name='catppuccin-mocha'
+
+    base = 
